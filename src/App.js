dfe321bc9c12abb1f6bd002a2ea3790cc51a353a
anwar-kalghoum <anwar.kalghoum@ensi-uma.tn>
2018-10-19 17:43:33 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { ReactiveBase, CategorySearch, ResultCard } from '@appbaseio/reactivesearch';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <ReactiveBase url="http://localhost:9200/" app="base" type="client">
+          
+            <div>
+            <h1>
+               Autocomplete ElasticSearch !
+            </h1>
+         
+              <CategorySearch
+                componentId="searchbox"
+                dataField="name"
+                categoryField="name.raw"
+                placeholder="Search for cars"
+              />
+              <ResultCard
+                componentId="searchbox"
+                dataField="name"
+                size={30}
+                pagination={true}
+                react={{
+                  and: ["searchbox", "ratingsfilter"]
+                }}
+                onData={(res) => {
+                  return {
+                    image: 'http://dummy-images.com/abstract/dummy-480x270-Utrecht.jpg',
+                    title: res.name,
+                    description: res.name + " " + "*".repeat(res.rating)
+                  }
+                }}
+              />
+				</div>
+          </ReactiveBase>
         </header>
       </div>
     );
